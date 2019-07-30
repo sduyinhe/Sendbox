68716d5c854b9ae79d9336ce005331d109ebf74e
yanghw <yanghw@cheche365.com>
2019-07-30 11:06:25 +0800

    2019年7月30日 init

---
+++ src/main/java/com/github/sendbox/SendboxApplication.java
@@ -0,0 +1,13 @@
+package com.github.sendbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendboxApplication.class, args);
+    }
+
+}
